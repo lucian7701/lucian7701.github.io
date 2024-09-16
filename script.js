fba20586352e63687455d74d10205475bca31f7f
lucian7701 <84269278+lucian7701@users.noreply.github.com>
2024-09-16 13:26:54 +0100

    feat: new message from LTJM, drop down subsections

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function toggleVisibility(id) {
-    var content = document.getElementById(id);
-    if (content.style.display === "none" || content.style.display === "") {
-      content.style.display = "block";
-    } else {
-      content.style.display = "none";
-    }
+function toggleVisibility(id, element) {
+  var content = document.getElementById(id);
+  var toggleState = element.getAttribute("data-toggle");
+
+  // Toggle content visibility
+  if (content.style.display === "none") {
+    content.style.display = "block";
+    element.setAttribute("data-toggle", "minus");
+    element.querySelector("span").innerText = "-";
+  } else {
+    content.style.display = "none";
+    element.setAttribute("data-toggle", "plus");
+    element.querySelector("span").innerText = "+";
   }
-  
+}
